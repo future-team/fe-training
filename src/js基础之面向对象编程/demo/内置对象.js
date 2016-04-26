d7a172740e2a20d2f56f6f21314bae14071cef48
slashhuang <627284482@qq.com>
2016-04-26 11:24:17 +0800

    adding internal OBj

--- src/js基础之面向对象编程/demo/内置对象.js
+++ src/js基础之面向对象编程/demo/内置对象.js
@@ -1,7 +1,7 @@
 /**
  * Created by slashhuang on 16/4/25.
  */
-
+var assert = require('assert');
 /**
  * ====== JS中的内置对象,方法不可变=======
  *
@@ -10,6 +10,7 @@
 
 /**
  * Math对象
+ * ===> 提供基本的数学函数如abs、sin等操作
  */
 var p = Math.random();
 console.log(p);
@@ -18,14 +19,14 @@ console.log(Math.round(2.3));
 
 /**
  * Number对象
- * ====>处理
+ * ====> 处理数据展示
  */
 var q= 1;  //等价于====> var p = new Number(1);
 console.log(q.toFixed(2));
 
 /**
- * Date 对象用于处理日期和时间。
- * =====> 经典的有moment时间函数库
+ * Date对象
+ * =====> 用于处理日期和时间。经典的有moment时间函数库
  */
 
 console.log(Date.now());
@@ -33,7 +34,28 @@ console.log(new Date().getDay());
 console.log(new Date().getDate());
 console.log(new Date().getMonth()+1);
 console.log(new Date().getFullYear());
-console.log(new Date().toDateString())
+console.log(new Date().toDateString());
+
+/**
+ * JSON对象
+ * ====> 轻量级的数据交换格式
+ */
+var normalObj={
+    json:'json'
+};
+//序列化一个JSON值
+var stringFy= JSON.stringify(normalObj);
+//反序列化一个JSON值
+var jsonToObj=JSON.parse(stringFy);
+console.log(stringFy);
+console.log(jsonToObj);
+
+/**
+ * 正则表达式
+ * ===>  匹配一系列符合某个句法规则的字符串
+ */
+var regExpEg= new RegExp(/\d/g);
+assert(true == regExpEg.test(1234))
 
 
 
