568341ae1b437404cc8a4793f127ade2975e88b1
amberonno <amberonno@gmail.com>
2016-04-28 16:08:59 +0800

    css基础之selector

---
+++ src/css基础之selector/demo/less_demo/webpack.config.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+    entry: "./index.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.less$/,
+                loader: "style!css!less"
+            }
+        ]
+    }
+};
